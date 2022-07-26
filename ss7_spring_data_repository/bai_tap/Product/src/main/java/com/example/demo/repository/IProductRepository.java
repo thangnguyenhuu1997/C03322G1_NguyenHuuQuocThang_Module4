8847a72e93b31b2a2ba628ab2f8df93b51758772
Your Name <you@example.com>
2022-07-26 08:26:13 +0700

    commit

--- ss7_spring_data_repository/bai_tap/Product/src/main/java/com/example/demo/repository/IProductRepository.java
+++ ss7_spring_data_repository/bai_tap/Product/src/main/java/com/example/demo/repository/IProductRepository.java
@@ -17,21 +17,21 @@ public interface IProductRepository extends JpaRepository<Product, Integer> {
     Page<Product> findAllProduct(Pageable pageable);
 
     @Modifying
-    @Query(value = "insert into product (name, price, country, status_delete) values (:name, :price, :country, 1)", nativeQuery = true)
+    @Query(value = "insert into product (  `name`, price, country, status_delete) values (:name, :price, :country, 1)", nativeQuery = true)
     void save(@Param("name") String name, @Param("price") Double price, @Param("country") String country);
 
     @Query(value = "select * from product where id = :id", nativeQuery = true)
     Product findByIdProduct(@Param("id") Integer id);
 
     @Modifying
-    @Query(value = "update product set name = :name, price = :price, country = :country where id = :id", nativeQuery = true)
+    @Query(value = "update product set `name` = :name, price = :price, country = :country where id = :id", nativeQuery = true)
     void update(@Param("name") String name, @Param("price") Double price, @Param("country") String country, @Param("id") Integer id);
 
     @Modifying
     @Query(value = "update product set status_delete = 0 where id = :id", nativeQuery = true)
     void delete(@Param("id") Integer id);
 
-    @Query(value = "select * from product where name = :name", nativeQuery = true,
+    @Query(value = "select * from product where `name` = :name", nativeQuery = true,
             countQuery="select count(*) from product where name = :name")
     Page<Product> findAllProductByName(Pageable pageable, @Param("name") String name);
 
